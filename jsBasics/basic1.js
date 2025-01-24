cad6d4e307ecd94b388aeb409c4b0c84925b5e55
Jatin Shekhawat <jatinsinghshekhawat33@gmail.com>
2025-01-24 17:12:27 +0530

    variables

---
+++ jsBasics/basic1.js
@@ -0,0 +1,17 @@
+const accountId = "1234";
+// cannot be reassigned once difined, its value cannot be change 
+
+let accountEmail = "jatinsinghshekhawat33@gamil.com";
+// cannot be re declared but can be reassigned
+
+var accountpassword = "123456";
+// can be re declared and update within its scope 
+
+// console.log(accountId);
+
+accountEmail = "jatin@gmail.com";
+console.log(accountEmail);
+
+// accountId = "321";
+// connot be reassigned 
+
