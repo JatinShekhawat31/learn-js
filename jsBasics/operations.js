fe912b7a9666f640cd109b9b6d4817eb48e9ea7e
Jatin Shekhawat <jatinsinghshekhawat33@gmail.com>
2025-01-24 17:51:05 +0530

    operations

---
+++ jsBasics/operations.js
@@ -0,0 +1,41 @@
+let value = 10;
+// show positive value
+let negValue = -10; 
+// show negavite value
+
+console.log(value, negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+// basic operations
+
+let str1 = 'Hello';
+let str2 = 'World';
+
+let str3 = str1 +" "+ str2;
+
+console.log(str3);
+
+console.log("1" + 1);
+// 11
+console.log(1 + "1");
+// 11
+console.log(1 + 1);
+// string operations
+console.log("1"+1+2);
+// 112
+console.log(1+1+"2");
+// 22
+console.log(+true);
+// 1
+console.log(+false);
+// 0
+
+console.log(1+"");  
+// 0 (not perfer to use)
+
+
