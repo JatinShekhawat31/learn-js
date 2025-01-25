28a61529c5bbf7c55cbc7e2a43e35653e6ba57f5
Jatin Shekhawat <jatinsinghshekhawat33@gmail.com>
2025-01-25 15:57:15 +0530

    strings

---
+++ jsBasics/strings.js
@@ -0,0 +1,15 @@
+const name = 'Jatin';
+const repoCount = 20;
+
+console.log(name + ' has ' + repoCount + ' repositories');
+// Jatin has 20 repositories
+
+const game = 'PUBG';
+console.log(game[0]);
+// p
+// p=0
+// u=1
+// b=2
+// g=3
+
+
