2f857db5d83d15236734326d7ab7324e19e1b515
JatinShekhawat31 <jatinsinghshekhawat33@gmail.com>
2025-01-31 12:46:07 +0530

    while and do while

---
+++ jsBasics/whileDo.js
@@ -0,0 +1,19 @@
+// WHILE LOOP
+
+let i = 0 
+while(i<=10)
+{
+    console.log(`value of i is ${i}`);
+    i=i+2
+}
+
+// USING ARRAY 
+
+let myArr = ['flash', 'superman', 'batman']
+let arr=0 
+while(arr < arr.length)
+{
+    console.log(`value of arr is ${myArr[arr]}`)
+}
+
+
